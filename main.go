cd036335712324887aa65ede47b64c3c17f76e16
schleising <stephen@schleising.net>
2022-05-22 18:35:55 +0100

    Initial triangle drawn

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strings"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -11,6 +12,32 @@ import (
 const (
 	width  = 800
 	height = 600
+
+	vertexShaderSource = `
+	#version 410 core
+    layout (location = 0) in vec3 aPos;
+    void main()
+    {
+       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+` + "\x00"
+
+	fragmentShaderSource = `
+	#version 410 core
+	out vec4 FragColor;
+	void main()
+	{
+		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	} 
+` + "\x00"
+)
+
+var (
+	triangle = []float32{
+		-0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0,
+		0.5, -0.5, 0.0,
+	}
 )
 
 func main() {
@@ -19,18 +46,38 @@ func main() {
 	window := initGlfw()
 	defer glfw.Terminate()
 
-	initOpenGL()
+	program := initOpenGL()
+
+	vao := makeVao(triangle)
 
 	for !window.ShouldClose() {
-		draw(window)
+		draw(window, program, vao)
 	}
 }
 
-func draw(window *glfw.Window) {
+func draw(window *glfw.Window, program uint32, vao uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT)
+	gl.UseProgram(program)
+	gl.BindVertexArray(vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, 3)
 
 	window.SwapBuffers()
 	glfw.PollEvents()
+}
+
+func makeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(points)*4, gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3 * 4, nil)
+	gl.EnableVertexAttribArray(0)
+
+	return vao
 }
 
 func initGlfw() *glfw.Window {
@@ -57,11 +104,31 @@ func initGlfw() *glfw.Window {
 	return window
 }
 
-func initOpenGL() {
+func initOpenGL() uint32 {
 	if err := gl.Init(); err != nil {
 		panic((err))
 	}
+
 	gl.ClearColor(0.118, 0.565, 1.0, 1.0)
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	gl.UseProgram(prog)
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return prog
 }
 
 func keyCallBack(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action, modifier glfw.ModifierKey) {
@@ -71,4 +138,27 @@ func keyCallBack(window *glfw.Window, key glfw.Key, scancode int, action glfw.Ac
 			window.SetShouldClose(true)
 		}
 	}
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
 }
