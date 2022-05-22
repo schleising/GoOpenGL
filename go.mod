67fe1374132d76f4d77b1ae9d6925c33dff3e9bc
schleising <stephen@schleising.net>
2022-05-22 15:17:13 +0100

    Create window that closes with ESC

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/schleising/GoOpenGL
 
 go 1.18
+
+require (
+	// github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220516021902-eb3e265c7661
+)
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
 
-func main()  {
-	fmt.Println("Test")
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+const(
+	width = 800
+	height = 600
+)
+
+func main() {
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+    for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+    }
+}
+
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Hello", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	glfw.SwapInterval(1)
+	window.SetKeyCallback(keyCallBack)
+
+	return window
+}
+
+func keyCallBack(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action, modifier glfw.ModifierKey) {
+	if action == glfw.Press {
+		fmt.Println(key)
+		if key == glfw.KeyEscape {
+			window.SetShouldClose(true)
+		}
+	}
 }
