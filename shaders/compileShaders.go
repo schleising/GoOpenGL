38e2a536dbf001115f300844f842335b651fa104
schleising <stephen@schleising.net>
2022-05-27 21:04:31 +0100

    Fix shader compile error message

--- shaders/compileShaders.go
+++ shaders/compileShaders.go
@@ -32,7 +32,7 @@ func LoadShader(filename string, shaderType uint32) (uint32, error) {
 		log := strings.Repeat("\x00", int(logLength+1))
 		gl.GetShaderInfoLog(handle, logLength, nil, gl.Str(log))
 
-		return 0, fmt.Errorf("failed to compile %v: %v", src, log)
+		return 0, fmt.Errorf("failed to compile %v: %v", filename, log)
 	}
 
 	return handle, nil
